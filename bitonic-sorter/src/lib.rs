9012db6f0079f35414ef1c8d6766433685772668
turkeyzawa <18262943+turkeyzawa@users.noreply.github.com>
2019-05-12 22:43:31 +0900

    init

---
+++ bitonic-sorter/src/lib.rs
@@ -0,0 +1,9 @@
+pub mod first;
+pub mod second;
+pub mod third;
+pub mod utils;
+
+pub enum SortOrder {
+    Ascending,
+    Descending,
+}
---
+++ bitonic-sorter/src/utils.rs
@@ -0,0 +1,12 @@
+use rand::{Rng, SeedableRng};
+use rand::distributions::Standard;
+use rand_pcg::Pcg64Mcg;
+
+pub fn new_u32_vec(n: usize) -> Vec<u32> {
+    let mut rng = Pcg64Mcg::from_seed([0; 16]);
+    let mut v = Vec::with_capacity(n);
+    for _ in 0..n {
+        v.push(rng.sample(&Standard));
+    };
+    v
+}
